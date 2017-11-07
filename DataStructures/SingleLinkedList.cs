a0244224b24fd3788b9e1d183f71c5966c49963a
LAPTOP-NT2BFFVB\markd <markda69@hotmail.com>
2017-11-06 20:36:59 -0800

    Single linked list - initial commit

---
+++ DataStructures/SingleLinkedList.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class SingleLinkedList<T>
+    {
+        public class Node
+        {
+            public T data;
+            public Node next;
+
+            public Node(T value)
+            {
+                data = value;
+                next = null;
+            }
+        }
+
+        public Node head;
+
+        // Insert data at beginning of list
+        public Node Insert(T data)
+        {
+            Node node = new Node(data);
+
+            node.next = head;
+            head = node;
+
+            return node;
+        }
+
+        // Add data to end of list
+        public Node Add(T data)
+        {
+            Node node = new Node(data);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node curr = head;
+                while (curr.next != null)
+                {
+                    curr = curr.next;
+                }
+
+                curr.next = node;
+            }
+
+            return node;
+        }
+
+        // Delete node from list
+        public Node Delete(Node node)
+        {
+            if (head == null
+             || node == null)
+            {
+                return null;
+            }
+
+            if (head == node)
+            {
+                head = node.next;
+                return node;
+            }
+
+            Node curr = head;
+            while (curr.next != null)
+            {
+                if (curr.next == node)
+                {
+                    curr.next = node.next;
+                    return node;
+                }
+                curr = curr.next;
+            }
+
+            return null;
+        }
+
+        public Node Find(Node node)
+        {
+            Node curr = head;
+            while (curr != null
+                && curr != node)
+            {
+                curr = curr.next;
+            }
+
+            return curr;
+        }
+
+        public Node FindNthToLast(int n)
+        {
+            // Advance ahead pointer
+            Node front = head;
+            for (int i=0; i<n; i++)
+            {
+                if (front != null)
+                {
+                    front = front.next;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            Node behind = head;
+            while (front != null)
+            {
+                front = front.next;
+                behind = behind.next;
+            }
+
+            return behind;
+        }
+    }
+}
---
+++ FindNthElement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures;
+
+namespace FindNthElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedList<string> list = new SingleLinkedList<string>();
+            list.Add("Hello");
+            list.Add("Brave");
+            SingleLinkedList<string>.Node del = list.Add("New");
+            list.Add("World");
+            list.Insert("Oh");
+            list.FindNthToLast(3);
+        }
+    }
+}
